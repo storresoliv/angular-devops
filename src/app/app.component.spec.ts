d0cbd0f6045e29c2e4428b527f23caecf91cb233
SATO <storresoliv@gmail.com>
2021-03-28 21:32:35 -0300

    add: contact form

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,32 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-devops'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-devops');
+    expect(app.title).toEqual('Angular devops');
   });
 
   it('should render subtitle', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.container--subtitle').textContent).toContain('a simple web app');
+    expect(compiled.querySelector('.container--subtitle').textContent).toContain('it\'s a simple web application');
+  });
+
+  it('should get name placeholder', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.componentInstance;
+    expect(app.namePlaceholder).toEqual('nombre');
+  });
+
+  it('should get email placeholder', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.componentInstance;
+    expect(app.emailPlaceholder).toEqual('correo');
+  });
+
+  it('should get send button text', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+  const compiled = fixture.nativeElement;
+  expect(compiled.querySelector('.button--success').textContent).toContain('ENVIAR');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-devops';
-  subtitle = 'a simple web app'
+  private _title = 'Angular devops';
+  private _subtitle = "it's a simple web application";
+  private _namePlaceholder = 'nombre';
+  private _emailPlaceholder = 'correo';
+  private _sendButtonText = 'ENVIAR';
+
+  public get title() {
+    return this._title;
+  }
+
+  public get subtitle(): string {
+    return this._subtitle;
+  }
+
+  public get namePlaceholder(): string {
+    return this._namePlaceholder;
+  }
+
+  public get emailPlaceholder(): string {
+    return this._emailPlaceholder;
+  }
+
+  public get sendButtonText(): string {
+    return this._sendButtonText;
+  }
 }
